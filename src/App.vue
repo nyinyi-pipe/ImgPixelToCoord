69587d31a7afce737d5bfe5c667a2c95afc128d9
nyinyi <nyinyi@pipe.bz>
2023-05-23 14:56:12 +0630

    Pixel to Coordinate in Console

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ImageCoordinateCalculator />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageCoordinateCalculator from './components/ImageCoordinateCalculator.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ImageCoordinateCalculator,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ImageCoordinateCalculator.vue
@@ -0,0 +1,70 @@
+<!-- <template>
+  <div>
+    <canvas ref="canvas" @click="calculateCoordinates"></canvas>
+  </div>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    this.canvas = this.$refs.canvas;
+    this.context = this.canvas.getContext('2d');
+    this.image = new Image();
+    this.image.src = ''; // Replace with your image path
+    this.image.onload = this.drawImage;
+  },
+  methods: {
+    drawImage() {
+      this.canvas.width = this.image.width;
+      this.canvas.height = this.image.height;
+      this.context.drawImage(this.image, 0, 0);
+    },
+    calculateCoordinates(event) {
+      const rect = this.canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      console.log(`Clicked coordinates: (${x}, ${y})`);
+    },
+  },
+};
+</script> -->
+
+
+
+<template>
+  <div>
+    <canvas ref="canvas" @click="calculateCoordinates"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.canvas = this.$refs.canvas;
+    this.context = this.canvas.getContext('2d');
+    this.image = new Image();    
+    this.image.src = '1/1.png'; // Replace with the actual path to your image file
+    this.image.onload = this.drawImage;
+  },
+  methods: {
+    drawImage() {
+      this.canvas.width = this.image.width;
+      this.canvas.height = this.image.height;
+      this.context.drawImage(this.image, 0, 0);
+    },
+    calculateCoordinates(event) {
+      const rect = this.canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      console.log(`Clicked coordinates: (${x}, ${y})`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+canvas {
+  border: 1px solid #000;
+}
+</style>
